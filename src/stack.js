db872f500a339f5d83113473d9006c9546321e92
n-andr <150958639+n-andr@users.noreply.github.com>
2025-02-25 23:49:17 +0100

    feat : add stack functionality

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  stackArr = []
+  push(element) {
+    this.stackArr.unshift(element);
+	return this.stackArr;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.stackArr.shift();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackArr[0];
   }
 }
 
